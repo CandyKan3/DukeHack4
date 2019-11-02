a69f453e5a388f66d1234808f86f8205675f424f
Bryce <bkane3@uncc.edu>
2019-11-02 17:36:42 -0400

    working

--- dukehack/src/components/main.jsx
+++ dukehack/src/components/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import logo from './banner.jpg';
 import real from './real.jpg';
+import talent from './talentshow.jpg'
 import Jumbotron from 'react-bootstrap/jumbotron';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
+import CardDeck from 'react-bootstrap/CardDeck';
+
 class Main extends React.Component {
   render() {
     return (
@@ -17,20 +20,34 @@ class Main extends React.Component {
          This webage was created to allow volunteers the ability to rideshare! For more information on what we do, click below
         </p>
         <p>
-          <Button variant="primary">Learn more</Button>
+          <Button variant="primary" href ="http://www.realityministriesinc.org">Learn more</Button>
         </p>
       </Jumbotron>
+      <CardDeck>
   <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={real} />
+  <Card.Img variant="top" />
   <Card.Body>
-    <Card.Title>Upcoming Events</Card.Title>
+    <Card.Title>Event Map</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      TODO: INSERT GOOGLE MAP WITH LOCATION
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
 </Card>
+<Card style={{ width: '18rem' }}>
+<Card.Img variant="top" src={talent} />
+<Card.Body>
+  <Card.Title>Talent Show</Card.Title>
+  <Card.Text>
+    SPECIAL EVENTS happen throughout
+    the year at Reality Ministries. Highlights include the
+     Talent Show and the Kings & Queens Winter Ball. See our current
+     calendar for specific events, dates, and times.
+  </Card.Text>
+  <Button variant="primary">Learn More</Button>
+</Card.Body>
+</Card>
+</CardDeck>
       </div>
     )
   }
