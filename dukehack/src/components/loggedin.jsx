5f61b8f94fce7be173b18a1f1fe6f2060c2a8de7
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2019-11-02 20:31:39 -0500

    Route Maps
    Route Maps

--- dukehack/src/components/loggedin.jsx
+++ dukehack/src/components/loggedin.jsx
@@ -6,21 +6,24 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Maps from "./maps";
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import MapComponent from "./route";
+import RoutComponent from "./route";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-      <Container>
-    <Row>
-      <Col>Info here</Col>
-      <Col>Map Here</Col>
-    </Row>
-    </Container>
-
+        <Container>
+          <Row>
+            <Col>Info here</Col>
+            <Col>
+              <RoutComponent lat="0" lng="0" zoom={8} />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
