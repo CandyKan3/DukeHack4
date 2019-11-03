957054ea45ccad424f9843ff7a464a614aee4cf2
Oliver King <oking3@uncc.edu>
2019-11-02 22:54:39 -0400

    Slightly changed server

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const API_PORT = 5000;
-const firebase = require('./database');
 
 // Get homepage
 router.get('/', function(req, res, next) {
@@ -18,18 +17,6 @@ router.post('/signup', function(req, res, next) {
   let email = req.body.email;
   let password = req.body.password;
 
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.error(
-        'Error:\nCode: ' + errorCode + '\nMessage: ' + errorMessage
-      );
-    });
-
   res.redirect('/login');
 });
 
@@ -38,17 +25,6 @@ router.post('/signin', function(req, res, next) {
   console.log(req.body);
   let email = req.body.email;
   let password = req.body.password;
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = errorMessage;
-      console.error(
-        'Error:\nCode: ' + errorCode + '\nMessage: ' + errorMessage
-      );
-    });
 
   res.redirect('/');
 });
@@ -66,24 +42,6 @@ router.post('/drive', function(req, res, next) {
   console.log(c);
   console.log(d);
   console.log(e);
-  var con = mysql.createConnection({
-    host: '-',
-    user: '-',
-    password: '-',
-    database: 'crest'
-  });
-
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected!');
-    var sql =
-      'INSERT INTO meetings(name, created, type, notes) VALUES (?,?,?,?)';
-    console.log(sql, d);
-    con.query(sql, [a, b, c, d], function(err, result) {
-      if (err) throw err;
-      console.log('1 record inserted');
-    });
-  });
   res.redirect('/login');
 });
 router.post('/ride', function(req, res, next) {
